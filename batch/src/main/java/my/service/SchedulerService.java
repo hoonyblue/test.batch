883fe6d57ff6ce64007a2e7243c8f1bdec621799
훈이 <@gnsl-pc>
2016-11-26 21:09:04 +0900

    jobScheduler 추가/삭제 ..

--- batch/src/main/java/my/service/SchedulerService.java
+++ batch/src/main/java/my/service/SchedulerService.java
@@ -75,7 +75,7 @@ public class SchedulerService {
 	 * @param jobId
 	 * @throws Exception
 	 */
-	public void delJob(String jobId) throws Exception {
+	public void removeJob(String jobId) throws Exception {
 		log.info("delJob : " + jobId);
 		
 		Scheduler scheduler = factory.getScheduler();
@@ -130,36 +130,105 @@ public class SchedulerService {
 	@Autowired
 	private JobRepository jobRepository; //JobRepositoryFactoryBean
 	
+	/**
+	 * 테스트 모듈..
+	 * @ses egov batch job + quartz(스케쥴)
+	 * @throws Exception
+	 */
 	public void test() throws Exception {
 		
 		String jobName = "ibatisToDelimitedJob";
 		String cronExpr = "0/10 * * * * ?";
 		
+		addJobSchedule(jobName, cronExpr);
+		
+//		JobDetailFactoryBean jobfactory = new JobDetailFactoryBean();
+//		jobfactory.setJobClass(EgovJobLauncherDetails.class);
+//		jobfactory.setGroup("quartz-batch");
+//		jobfactory.setName("ibatisToDelimitedJob");
+//		
+//		Map<String, Object> jobDataAsMap = new Hashtable<String, Object>();
+//		jobDataAsMap.put("jobName", jobName);
+//		jobDataAsMap.put("jobLocator", jobRegistry);
+//		jobDataAsMap.put("jobLauncher", jobLauncher);
+//		jobfactory.setJobDataAsMap(jobDataAsMap);
+//		jobfactory.afterPropertiesSet();
+//		JobDetail jobDetail = jobfactory.getObject();
+//		log.info(String.format("jobDetail : %s", jobDetail));
+//		log.info(String.format("jobName : %s", jobName));
+//		
+//		org.springframework.batch.core.job.SimpleJob job = new org.springframework.batch.core.job.SimpleJob(jobName);
+//		job.setJobRepository((JobRepository) jobRepository);
+//		ReferenceJobFactory jobFactory = new ReferenceJobFactory((org.springframework.batch.core.Job)job);
+//		jobRegistry.register(jobFactory);
+//		
+//		Trigger trigger = newTrigger(jobDetail, jobName, cronExpr);		
+//		Scheduler scheduler = factory.getScheduler();
+//		scheduler.scheduleJob(jobDetail, trigger);
+		
+	}
+	
+	/**
+	 * job scheduler 등록. (egov batch)
+	 * @param jobId
+	 * @param cronExpr <expression :  예) 0/10 * * * * ?  => 10초마다. 
+	 * @throws Exception
+	 */
+	public void addJobSchedule(String jobId, String cronExpr) throws Exception {
+
 		JobDetailFactoryBean jobfactory = new JobDetailFactoryBean();
 		jobfactory.setJobClass(EgovJobLauncherDetails.class);
 		jobfactory.setGroup("quartz-batch");
-		jobfactory.setName("ibatisToDelimitedJob");
-		
+		jobfactory.setName(jobId);
+
 		Map<String, Object> jobDataAsMap = new Hashtable<String, Object>();
-		jobDataAsMap.put("jobName", jobName);
+		jobDataAsMap.put("jobName", jobId);
 		jobDataAsMap.put("jobLocator", jobRegistry);
 		jobDataAsMap.put("jobLauncher", jobLauncher);
 		jobfactory.setJobDataAsMap(jobDataAsMap);
 		jobfactory.afterPropertiesSet();
 		JobDetail jobDetail = jobfactory.getObject();
-		log.info(String.format("jobDetail : %s", jobDetail));
-		log.info(String.format("jobName : %s", jobName));
+		log.debug(String.format("jobDetail : %s", jobDetail));
+		log.debug(String.format("jobName : %s", jobId));
 		
-		org.springframework.batch.core.job.SimpleJob job = new org.springframework.batch.core.job.SimpleJob(jobName);
+		org.springframework.batch.core.job.SimpleJob job = new org.springframework.batch.core.job.SimpleJob(jobId);
 		job.setJobRepository((JobRepository) jobRepository);
 		ReferenceJobFactory jobFactory = new ReferenceJobFactory((org.springframework.batch.core.Job)job);
 		jobRegistry.register(jobFactory);
 		
-		Trigger trigger = newTrigger(jobDetail, jobName, cronExpr);		
+		Trigger trigger = newTrigger(jobDetail, jobId, cronExpr);		
 		Scheduler scheduler = factory.getScheduler();
 		scheduler.scheduleJob(jobDetail, trigger);
-		//scheduler.scheduleJob(trigger);
-		
+
+	}
+	
+	public void removeJobSchedule(String jobId) throws Exception {
+
+		Scheduler scheduler = factory.getScheduler();
+		boolean _chk = scheduler.checkExists(JobKey.jobKey(jobId));
+		log.debug(String.format("check_job: %s",_chk));
+		if (_chk) {
+			scheduler.deleteJob(JobKey.jobKey(jobId));
+		} else {
+			log.info(String.format("jobId: %s is not existed.", jobId));
+		}
+		boolean _chk2 = scheduler.checkExists(TriggerKey.triggerKey(jobId));
+		log.debug(String.format("check_trigger:", _chk2));
+		if (_chk2) 
+			scheduler.unscheduleJob(TriggerKey.triggerKey(jobId));
+		// jobRegistry 제거.
+		jobRegistry.unregister(jobId);
+	}
+	
+	/**
+	 * trigger 갱신 (삭제 후, 재등록)
+	 * @param jobId
+	 * @param cronExpr
+	 * @throws Exception
+	 */
+	public void updateJobSchedule(String jobId, String cronExpr) throws Exception {
+		removeJobSchedule(jobId);
+		addJobSchedule(jobId, cronExpr);	
 		
 	}
 }
