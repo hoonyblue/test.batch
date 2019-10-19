65194f019c573748f9e46f0e798c008b7858f8ba
훈이 <@gnsl-pc2>
2019-10-19 15:10:17 +0900

    MacBnd 수정.

--- src/main/java/my/controller/MyController.java
+++ src/main/java/my/controller/MyController.java
@@ -11,30 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-	
+
 	@Autowired
 	SchedulerService service;
 
 	private static Logger log = Logger.getLogger(MyController.class);
-	
+
 	@RequestMapping("/")
 	public @ResponseBody String index() {
-		
+
 		log.info("test");
-		
+
 		try {
-			service.removeJob(SampleJob2.class.getName()); //"SampleJob2");
+			service.removeJob(SampleJob2.class.getName()); //"SampleJob2")
 			log.info("del job");
 			String _addjob = service.addJob("my.job.SampleJob2", SampleJob2.class.getName(), "0/10 * * * * ?");
 			log.info("add job: " + _addjob);
-			
+
 			service.test();
-			
+
 		} catch(Exception e) {
 			log.error("error", e);
 		}
-		
+
 		return "Hello Woniper Spring Boot~";
 	}
-	
+
 }
